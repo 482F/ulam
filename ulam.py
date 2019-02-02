df40480211f546e211002dbb11d6be8b1f762891
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-02-02 09:13:26 +0900

    mr, mc を mx, my に変更

--- ulam.py
+++ ulam.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 def NoD_list(mn):
     num_list = [1 for x in range(mn)]
     num_list[0] = 0
@@ -9,14 +12,14 @@ def NoD_list(mn):
     #mn = 10 -> [0, 1, 1, 2, 1, 3, 1, 2, 2,  3]
     #           [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def start_pos(mr, mc):
-    return [mr - 1, mc - 1]
+def start_pos(mx, my):
+    return [mx - 1, my - 1]
 
-def make_direction(mr, mc):
+def make_direction(mx, my):
     return [-1, 0]
 
-def make_width(mr, mc):
-    return [mr - 1, mc - 1]
+def make_width(mx, my):
+    return [mx - 1, my - 1]
 
 def next_direction(current_direction):
     current_direction = tuple(current_direction)
@@ -27,16 +30,16 @@ def next_direction(current_direction):
             (0, 1): [-1, 0],
             }
     return direction_dict[current_direction]
-def pos_list(mr, mc):
+def pos_list(mx, my):
     num = 1
-    pos = start_pos(mr, mc)
+    pos = start_pos(mx, my)
     pos_list = [pos[:]]
-    direction = make_direction(mr, mc)
-    width = make_width(mr, mc)
+    direction = make_direction(mx, my)
+    width = make_width(mx, my)
     current_width = width[:]
     width[0] += 1
     index = 0
-    while (num <= (mr * mc) - 1):
+    while (num <= (mx * my) - 1):
         if (current_width[index] == 0):
             width[index] -= 1
             current_width = width[:]
